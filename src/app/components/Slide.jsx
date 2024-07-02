89de8260c405d3251b01312ee3b1cb7d4230d490
AymanMohammedGo <ayhammohammed723@gmail.com>
2024-07-02 13:17:59 +0300

    adding hover for image

--- src/app/components/Slide.jsx
+++ src/app/components/Slide.jsx
@@ -43,67 +43,157 @@ const MySwiper = () => {
         modules={[Pagination]}
         ref={swiperRef}
         spaceBetween={30}
-        slidesPerView={4}
+        slidesPerView={2}
         centeredSlides={true}
         pagination={{
           type: "progressbar",
         }}
+        breakpoints={{
+          824: {
+            slidesPerView: 4,
+          },
+          1600: {
+            slidesPerView: 6,
+          },
+        }}
       >
         <SwiperSlide>
-          <Image
-            className="rounded-lg
-
-"
-            src="/assets/logo.jpg"
-            alt="brand"
-            width={400}
-            height={200}
-          />
+          <div className="relative">
+            <Image
+              className="rounded-lg "
+              src="/assets/logo.jpg"
+              alt="brand"
+              width={400}
+              height={200}
+            />
+            <div className="absolute rounded-lg top-0 left-0 w-full h-full flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-300">
+              <Image
+                className="rounded-lg"
+                src="/assets/hover.jpg"
+                alt="hovered image"
+                width={400}
+                height={200}
+              />
+              <div className=" py-3 px-4 absolute bottom-0 left-0 text-black text-lg font-bold">
+                Architectural
+              </div>
+            </div>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-          <Image
-            className="rounded-lg"
-            src="/assets/logo.jpg"
-            alt="brand"
-            width={400}
-            height={200}
-          />
-        </SwiperSlide>
+          <div className="relative">
+            <Image
+              className="rounded-lg "
+              src="/assets/logo.jpg"
+              alt="brand"
+              width={400}
+              height={200}
+            />
+            <div className="absolute rounded-lg top-0 left-0 w-full h-full flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-300">
+              <Image
+                className="rounded-lg"
+                src="/assets/hover.jpg"
+                alt="hovered image"
+                width={400}
+                height={200}
+              />
+              <div className=" py-3 px-4 absolute bottom-0 left-0 text-black text-lg font-bold">
+                Architectural
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>{" "}
         <SwiperSlide>
-          <Image
-            className="rounded-lg"
-            src="/assets/logo.jpg"
-            alt="brand"
-            width={400}
-            height={200}
-          />
-        </SwiperSlide>
+          <div className="relative">
+            <Image
+              className="rounded-lg "
+              src="/assets/logo.jpg"
+              alt="brand"
+              width={400}
+              height={200}
+            />
+            <div className="absolute rounded-lg top-0 left-0 w-full h-full flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-300">
+              <Image
+                className="rounded-lg"
+                src="/assets/hover.jpg"
+                alt="hovered image"
+                width={400}
+                height={200}
+              />
+              <div className=" py-3 px-4 absolute bottom-0 left-0 text-black text-lg font-bold">
+                Architectural
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>{" "}
         <SwiperSlide>
-          <Image
-            className="rounded-lg"
-            src="/assets/logo.jpg"
-            alt="brand"
-            width={400}
-            height={200}
-          />
-        </SwiperSlide>
+          <div className="relative">
+            <Image
+              className="rounded-lg "
+              src="/assets/logo.jpg"
+              alt="brand"
+              width={400}
+              height={200}
+            />
+            <div className="absolute rounded-lg top-0 left-0 w-full h-full flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-300">
+              <Image
+                className="rounded-lg"
+                src="/assets/hover.jpg"
+                alt="hovered image"
+                width={400}
+                height={200}
+              />
+              <div className=" py-3 px-4 absolute bottom-0 left-0 text-black text-lg font-bold">
+                Architectural
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>{" "}
         <SwiperSlide>
-          <Image
-            className="rounded-lg"
-            src="/assets/logo.jpg"
-            alt="brand"
-            width={400}
-            height={200}
-          />
-        </SwiperSlide>
+          <div className="relative">
+            <Image
+              className="rounded-lg "
+              src="/assets/logo.jpg"
+              alt="brand"
+              width={400}
+              height={200}
+            />
+            <div className="absolute rounded-lg top-0 left-0 w-full h-full flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-300">
+              <Image
+                className="rounded-lg"
+                src="/assets/hover.jpg"
+                alt="hovered image"
+                width={400}
+                height={200}
+              />
+              <div className=" py-3 px-4 absolute bottom-0 left-0 text-black text-lg font-bold">
+                Architectural
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>{" "}
         <SwiperSlide>
-          <Image
-            className="rounded-lg"
-            src="/assets/logo.jpg"
-            alt="brand"
-            width={400}
-            height={200}
-          />
+          <div className="relative">
+            <Image
+              className="rounded-lg "
+              src="/assets/logo.jpg"
+              alt="brand"
+              width={400}
+              height={200}
+            />
+            <div className="absolute rounded-lg top-0 left-0 w-full h-full flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-300">
+              <Image
+                className="rounded-lg"
+                src="/assets/hover.jpg"
+                alt="hovered image"
+                width={400}
+                height={200}
+              />
+              <div className=" py-3 px-4 absolute bottom-0 left-0 text-black text-lg font-bold">
+                Architectural
+              </div>
+            </div>
+          </div>
         </SwiperSlide>
       </Swiper>
     </div>

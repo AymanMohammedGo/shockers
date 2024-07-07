a59a4b0a83a05dac4d8c31c34ae69c7e81f8ec51
AymanMohammedGo <ayhammohammed723@gmail.com>
2024-07-07 22:19:07 +0300

    fix language and do public for all page

--- src/app/[locale]/layout.jsx
+++ src/app/[locale]/layout.jsx
@@ -2,8 +2,11 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import i18nConfig from "../../../i18nConfig";
 import { dir } from "i18next";
+import TranslationsProvider from "@/components/TranslationsProvider";
+import initTranslations from "../i18n";
 
 const inter = Inter({ subsets: ["latin"] });
+const i18nNamespaces = ["Home"];
 
 export const metadata = {
   title: "Shockers Group",
@@ -12,10 +15,18 @@ export const metadata = {
 export function generateStaticParams() {
   return i18nConfig.locales.map((locale) => ({ locale }));
 }
-export default function RootLayout({ children, params: { locale } }) {
+export default async function RootLayout({ children, params: { locale } }) {
+  const { t, resources } = await initTranslations(locale, i18nNamespaces);
+
   return (
-    <html lang={locale} dir={dir(locale)}>
-      <body className={inter.className}>{children}</body>
-    </html>
+    <TranslationsProvider
+      resources={resources}
+      locale={locale}
+      namespaces={i18nNamespaces}
+    >
+      <html lang={locale} dir={dir(locale)}>
+        <body className={inter.className}>{children}</body>
+      </html>
+    </TranslationsProvider>
   );
 }
--- src/app/[locale]/page.jsx
+++ src/app/[locale]/page.jsx
@@ -1,22 +1,11 @@
-import TranslationsProvider from "@/components/TranslationsProvider";
 import HeaderBrond from "../../components/BrondHome/HeaderBrond";
 import MySwiper from "../../components/BrondHome/SlideBrond";
-import initTranslations from "../i18n";
 
-const i18nNamespaces = ["Home"];
-export default async function Home({ params: { locale } }) {
-  const { t, resources } = await initTranslations(locale, i18nNamespaces);
-
+export default async function Home() {
   return (
-    <TranslationsProvider
-      resources={resources}
-      locale={locale}
-      namespaces={i18nNamespaces}
-    >
-      <main className="h-screen overflow-hidden bg-slate-950">
-        <HeaderBrond />
-        <MySwiper />
-      </main>
-    </TranslationsProvider>
+    <main className="h-screen overflow-hidden bg-slate-950">
+      <HeaderBrond />
+      <MySwiper />
+    </main>
   );
 }

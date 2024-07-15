106299061be3b09ae42f3bb352aef5ab7cb60aa4
AymanMohammedGo <ayhammohammed723@gmail.com>
2024-07-15 10:08:57 +0300

    add slide vertical

--- src/app/[locale]/shockersAEC/projects/page.jsx
+++ src/app/[locale]/shockersAEC/projects/page.jsx
@@ -1,18 +1,42 @@
+"use client";
 import ImageOverlaysTop from "@/components/ImageOverlaysTop";
 import ImageOverlaysCenter from "@/components/ImageOverlaysCenter";
 import SlideProject from "@/components/SlideProject";
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+import { Pagination } from "swiper/modules";
 
 const Projects = () => {
   return (
     <div>
       <ImageOverlaysTop namePage="PROJECTS" title="Shockers AEC" />
-      <section className=" w-full ">
-        <ImageOverlaysCenter title=" PROJECTS 1 " />
-        <ImageOverlaysCenter title=" PROJECTS 2" />
-        <ImageOverlaysCenter title=" PROJECTS 3" />
-        <ImageOverlaysCenter title=" PROJECTS 4" />
-      </section>
+      {/* <section className=" w-full h-full"></section> */}
+      <Swiper
+        direction={"vertical"}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="w-full  h-[600px] "
+      >
+        <SwiperSlide>
+          <ImageOverlaysCenter title=" PROJECTS 1 " />
+        </SwiperSlide>
+        <SwiperSlide>
+          <ImageOverlaysCenter title=" PROJECTS 2" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <ImageOverlaysCenter title=" PROJECTS 3" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <ImageOverlaysCenter title=" PROJECTS 4" />
+        </SwiperSlide>
+      </Swiper>
       <h2 className="font-bold  text-seconds  text-3xl text-center my-12">
         PROJECTS
       </h2>

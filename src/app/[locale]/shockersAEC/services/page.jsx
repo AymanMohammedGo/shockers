d7ef20fe6c68bc89a00babed6113f399ce51d94f
AymanMohammedGo <ayhammohammed723@gmail.com>
2024-07-17 12:48:20 +0300

    adding page services

--- src/app/[locale]/shockersAEC/services/page.jsx
+++ src/app/[locale]/shockersAEC/services/page.jsx
@@ -12,11 +12,13 @@ const Services = () => {
   return (
     <>
       <ImageOverlaysTop namePage="SERVICES" title="Shockers AEC" />
-      <div className="max-w-screen-xxl m-auto w-full my-6 p-2">
+      <div className="max-w-screen-xxl m-auto w-full  p-2">
         <Accordion type="single" collapsible className="w-full">
-          <AccordionItem value="item-1">
-            <AccordionTrigger>Architectural Services</AccordionTrigger>
-            <AccordionContent>
+          <AccordionItem value="item-1" className="my-5">
+            <AccordionTrigger className="font-bold text-2xl md:text-4xl text-seconds">
+              Architectural Services
+            </AccordionTrigger>
+            <AccordionContent className="text-xl ">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea illo
               est doloremque perferendis iure enim totam unde ullam temporibus,
               earum eveniet laborum commodi tempore sequi consectetur id
@@ -24,38 +26,79 @@ const Services = () => {
               adipisicing elit. Voluptate recusandae assumenda delectus, quidem
               soluta ipsum? Adipisci, maiores quisquam ducimus praesentium esse
               corrupti alias laudantium, possimus necessitatibus rerum quae
-              architecto? Dolores.
+              architecto? Dolores.Lorem ipsum dolor, sit amet consectetur
+              adipisicing elit. Ea illo est doloremque perferendis iure enim
+              totam unde ullam temporibus, earum eveniet laborum commodi tempore
+              sequi consectetur id deserunt, assumenda iste. Lorem ipsum dolor
+              sit amet consectetur adipisicing elit. Voluptate recusandae
+              assumenda delectus, quidem soluta ipsum? Adipisci, maiores
+              quisquam ducimus praesentium esse corrupti alias laudantium,
+              possimus necessitatibus rerum quae architecto? Dolores.
               <Image
-                className=" w-full"
+                className=" w-full my-3 h-[600px] object-cover"
                 src="/assets/services.jpg"
-                width={500}
-                height={500}
+                width={1000}
+                height={200}
                 alt="project"
               />
             </AccordionContent>
           </AccordionItem>
-          <AccordionItem value="item-2">
-            <AccordionTrigger>Engineering Services</AccordionTrigger>
-            <AccordionContent>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus
-              ea fugit at ab laudantium facere voluptatibus autem amet voluptas
-              aperiam cumque fuga in ad, recusandae accusamus voluptates
-              inventore itaque optio! Lorem ipsum, dolor sit amet consectetur
-              adipisicing elit. Consectetur optio dolor, at est exercitationem
-              sit impedit aliquid minus fugiat hic corrupti distinctio possimus
-              dolore fuga inventore fugit pariatur aliquam architecto.
+          <AccordionItem value="item-2" className="my-5">
+            <AccordionTrigger className="font-bold text-2xl md:text-4xl text-seconds">
+              Engineering Services
+            </AccordionTrigger>
+            <AccordionContent className="text-xl ">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea illo
+              est doloremque perferendis iure enim totam unde ullam temporibus,
+              earum eveniet laborum commodi tempore sequi consectetur id
+              deserunt, assumenda iste. Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Voluptate recusandae assumenda delectus, quidem
+              soluta ipsum? Adipisci, maiores quisquam ducimus praesentium esse
+              corrupti alias laudantium, possimus necessitatibus rerum quae
+              architecto? Dolores.Lorem ipsum dolor, sit amet consectetur
+              adipisicing elit. Ea illo est doloremque perferendis iure enim
+              totam unde ullam temporibus, earum eveniet laborum commodi tempore
+              sequi consectetur id deserunt, assumenda iste. Lorem ipsum dolor
+              sit amet consectetur adipisicing elit. Voluptate recusandae
+              assumenda delectus, quidem soluta ipsum? Adipisci, maiores
+              quisquam ducimus praesentium esse corrupti alias laudantium,
+              possimus necessitatibus rerum quae architecto? Dolores.
+              <Image
+                className=" w-full my-3 h-[600px] object-cover"
+                src="/assets/services.jpg"
+                width={1000}
+                height={200}
+                alt="project"
+              />
             </AccordionContent>
           </AccordionItem>
-          <AccordionItem value="item-3">
-            <AccordionTrigger>Construction Services</AccordionTrigger>
-            <AccordionContent>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius
-              sapiente ipsam repellendus similique, eos accusamus sunt magnam
-              necessitatibus consequatur non dolorem nisi quas adipisci
-              provident maiores dignissimos deleniti possimus. A. Lorem ipsum,
-              dolor sit amet consectetur adipisicing elit. Doloremque minus unde
-              dolor rem officia esse quod dolorum ea id tempora, eveniet in
-              accusamus libero ipsa consectetur aliquid explicabo illo possimus.
+          <AccordionItem value="item-3" className="my-5">
+            <AccordionTrigger className="font-bold text-2xl md:text-4xl text-seconds">
+              Construction Services
+            </AccordionTrigger>
+            <AccordionContent className="text-xl ">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea illo
+              est doloremque perferendis iure enim totam unde ullam temporibus,
+              earum eveniet laborum commodi tempore sequi consectetur id
+              deserunt, assumenda iste. Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Voluptate recusandae assumenda delectus, quidem
+              soluta ipsum? Adipisci, maiores quisquam ducimus praesentium esse
+              corrupti alias laudantium, possimus necessitatibus rerum quae
+              architecto? Dolores.Lorem ipsum dolor, sit amet consectetur
+              adipisicing elit. Ea illo est doloremque perferendis iure enim
+              totam unde ullam temporibus, earum eveniet laborum commodi tempore
+              sequi consectetur id deserunt, assumenda iste. Lorem ipsum dolor
+              sit amet consectetur adipisicing elit. Voluptate recusandae
+              assumenda delectus, quidem soluta ipsum? Adipisci, maiores
+              quisquam ducimus praesentium esse corrupti alias laudantium,
+              possimus necessitatibus rerum quae architecto? Dolores.
+              <Image
+                className=" w-full my-3 h-[600px] object-cover"
+                src="/assets/services.jpg"
+                width={1000}
+                height={200}
+                alt="project"
+              />
             </AccordionContent>
           </AccordionItem>
         </Accordion>

58c80cffc47f485b9c40f068ea8439affead03a4
AymanMohammedGo <ayhammohammed723@gmail.com>
2024-07-17 13:20:31 +0300

    adding video aec

--- src/app/[locale]/shockersAEC/layout.jsx
+++ src/app/[locale]/shockersAEC/layout.jsx
@@ -1,12 +1,23 @@
+"use client";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import LoadingVideo from "@/components/LoadingVideo";
+import { useState } from "react";
 
-export default async function RootLayout({ children }) {
+export default function RootLayout({ children }) {
+  const [isVideoEnded, setIsVideoEnded] = useState(true);
+
   return (
     <div className="bg-primary min-h-screen flex flex-col justify-between ">
-      <Header />
-      {children}
-      <Footer />
+      {/* {isVideoEnded ? (
+        <LoadingVideo URL="/done8_2.mp4" setIsVideoEnded={setIsVideoEnded} />
+      ) : ( */}
+      <>
+        <Header />
+        {children}
+        <Footer />
+      </>
+      {/* )} */}
     </div>
   );
 }

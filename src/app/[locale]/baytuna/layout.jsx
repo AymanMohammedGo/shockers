dde77e307eb77364d3e93aa575dff2384acbe102
AymanMohammedGo <ayhammohammed723@gmail.com>
2024-07-31 12:45:38 +0300

    fix deploy

--- src/app/[locale]/baytuna/layout.jsx
+++ src/app/[locale]/baytuna/layout.jsx
@@ -3,7 +3,7 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import LoadingVideo from "@/components/LoadingVideo";
 import { useEffect, useState, useCallback } from "react";
-import Baytuna from "../../../../public/Motion/Baytuna.json";
+import Baytuna from "/public/Motion/Baytuna.json";
 import Transition from "@/components/Motion/Transition";
 import DrawLogo from "@/components/Lottie/DrawLogo";
 

6ee88578c055b8f3725aab41a8bc88640219e9ca
AymanMohammedGo <ayhammohammed723@gmail.com>
2024-08-01 16:22:07 +0300

    change about

--- src/app/[locale]/shockersAEC/about/page.jsx
+++ src/app/[locale]/shockersAEC/about/page.jsx
@@ -33,50 +33,6 @@ const About = ({ params: { locale } }) => {
     getTopAbout_();
   }, [getAboutUS_, getTopAbout_]);
 
-  // const aboutus = [
-  //   {
-  //     title: "About ShockersAEC",
-  //     des: "Shockers AEC is a full-service firm encompassing architecture, engineering, and construction. Through collaborative practices and a dedication to innovation, they serve as a leading force in shaping the built environment",
-  //     image: "/img/services.jpg",
-  //     x: "-100%",
-  //     left: "xl:flex-row",
-  //   },
-  //   {
-  //     title: "Years of experience and performance",
-  //     des: "Since 2017, our architects, engineers, and managers teams have grown through dedication and continuous learning. We combine experience and creativity, transforming your vision into a remarkable reality. Every project reflects our commitment to top quality and designs that impress and inspire every client.",
-  //     image: "/img/services.jpg",
-  //     x: "100%",
-  //     left: "xl:flex-row-reverse",
-  //   },
-  //   {
-  //     title: "Mission",
-  //     des: "Our mission is to enhance society through the application of innovative architectural designs that embody imaginative and creative thinking. Our team of skilled architects is dedicated to unlocking the full potential of your projects, offering solutions even for the most intricate design challenges. By providing creative and effective solutions, we strive to enhance the lives and businesses of our clients.",
-  //     image: "/img/services.jpg",
-  //     x: "-100%",
-  //     left: "xl:flex-row",
-  //   },
-  //   {
-  //     title: "Vision",
-  //     des: "Our vision is to deliver exceptional design solutions of the highest quality by leveraging our imaginative visualization skills and transforming them into tangible reality. We aim to create captivating and innovative designs that inspire and surpass expectations, leaving a lasting impact on the built environment.",
-  //     image: "/img/services.jpg",
-  //     x: "100%",
-  //     left: "xl:flex-row-reverse",
-  //   },
-  //   {
-  //     title: "Value and quality",
-  //     des: "At Shockers AEC, integrity guides everything. Honesty, trust, and commitment to excellence fuel our passion for continuous learning and innovation. We value diversity and teamwork, striving for constant improvement to deliver superior results for every client.",
-  //     image: "/img/services.jpg",
-  //     x: "-100%",
-  //     left: "xl:flex-row",
-  //   },
-  //   {
-  //     title: "Staff",
-  //     des: "Our team is led by highly skilled architectural, engineering, and construction design teams. we are constantly expanding across all the departments to meet our targets. ",
-  //     image: "/img/services.jpg",
-  //     x: "100%",
-  //     left: "xl:flex-row-reverse",
-  //   },
-  // ];
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -98,60 +54,33 @@ const About = ({ params: { locale } }) => {
             key={index}
             className={`w-screen h-screen  flex flex-col items-center ${
               index % 2 === 0 ? "xl:flex-row" : "xl:flex-row-reverse"
-            }   justify-center lg:justify-between`}
+            } justify-center lg:justify-between`}
           >
-            <div className="p-2 lg:p-24 xl:p-32  text-shockersAEC flex-1 flex flex-col items-start">
-              <span className="font-bold text-4xl md:text-5xl lg:text-6xl my-3  lg:mb-6">
+            <div className="p-4 lg:p-24 xl:p-32  text-shockersAEC flex-1 flex flex-col items-start">
+              <h2 className="font-bold text-4xl md:text-5xl lg:text-6xl my-3  lg:mb-6 !leading-[50px] lg:!leading-[70px]">
                 {item?.attributes.title}
-              </span>
-              <p className="text-xl lg:text-2xl mb-3 lg:mb-0">
+              </h2>
+              <p className="text-xl lg:text-2xl mb-3 lg:mb-0 !leading-8 lg:!leading-10 text-justify">
                 {item?.attributes.description}
               </p>
             </div>
-            <div className="relative w-screen h-full xl:h-screen xl:w-[50vw] ">
+            <div className="relative p-4 w-full h-full xl:h-screen xl:w-[50vw] flex items-center justify-center   ">
               <Image
-                src={item?.attributes.imgURL.data?.attributes.url}
-                fill={true}
+                src="/img/about.svg"
                 alt="aboutUs"
                 quality={75}
-                className="object-cover"
+                width={500}
+                height={500}
+                // className="object-cover"
               />
             </div>
           </motion.div>
         ))}
       </section>
-      {/* <section className="overflow-hidden">
-        {aboutus.map((item, index) => (
-          <motion.div
-            initial={{ x: item.x, opacity: 0 }}
-            whileInView={{ x: 0, opacity: 1 }}
-            transition={{
-              duration: 1,
-            }}
-            key={index}
-            className={`w-screen h-screen  flex flex-col items-center ${item.left} justify-center lg:justify-between`}
-          >
-            <div className="p-2 md:p-4 lg:p-16 xl:p-28 xxl:p-32 text-shockersAEC flex-1 flex flex-col items-start">
-              <span className="font-bold text-4xl md:text-5xl lg:text-6xl my-3  lg:mb-6">
-                {item.title}
-              </span>
-              <p className="text-xl lg:text-2xl mb-3 lg:mb-0">{item.des}</p>
-            </div>
-            <div className="relative w-screen h-full xl:h-screen xl:w-[50vw] ">
-              <Image
-                src={item.image}
-                fill={true}
-                alt="aboutus"
-                quality={75}
-                className="object-cover"
-              />
-            </div>
-          </motion.div>
-        ))}
-      </section> */}
-      <Link href="/shockersAEC/projects">
+
+      {/* <Link href="/shockersAEC/projects">
         <ImageOverlaysCenter title="VIEW OUR PROJECTS" />
-      </Link>
+      </Link> */}
     </motion.div>
   );
 };
--- src/app/[locale]/shockersAEC/services/page.jsx
+++ src/app/[locale]/shockersAEC/services/page.jsx
@@ -72,7 +72,7 @@ const Services = ({ params: { locale } }) => {
       />
 
       <div className="flex flex-col md:flex-row min-h-screen lg:py-5 max-w-screen-xxl m-auto relative z-10">
-        <div className="md:w-1/3 p-2  md:p-4">
+        <div className="md:w-1/3 p-2  pt-8 md:p-4">
           <ul className="space-y-2">
             {/* {servicesData.map((service, index) => (
               <li
@@ -108,7 +108,7 @@ const Services = ({ params: { locale } }) => {
           </ul>
         </div>
         <div className="md:w-2/3 p-2 md:p-4">
-          <p className="mb-6 text-xl lg:text-2xl !leading-10  py-4 px-4 lg:px-10 lg:text-justify">
+          <p className="mb-6 text-xl lg:text-2xl !leading-10  py-4 px-4 lg:px-10 text-justify">
             {/* {selectedService.description} */}
             {selectedService?.attributes?.description}
           </p>

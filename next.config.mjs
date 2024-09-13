44b1a2767433422be13c48cef3106415e95d17d7
AymanMohammedGo <ayhammohammed723@gmail.com>
2024-09-13 10:54:41 +0300

    change cms

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "glorious-prosperity-1a9651c49e.media.strapiapp.com",
+        hostname: "determined-bat-94d48cd643.media.strapiapp.com",
         port: "", // يمكنك ترك هذا الحقل فارغًا إذا لم يكن هناك منفذ معين
         pathname: "/**", // هذا النمط يسمح بجلب الصور من جميع المسارات ضمن هذا النطاق
       },
--- utils/axiosClient.js
+++ utils/axiosClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: "http://localhost:1337/api",
-  baseURL: "https://glorious-prosperity-1a9651c49e.strapiapp.com/api",
+  baseURL: "https://determined-bat-94d48cd643.strapiapp.com/api",
 });
 
 export default instance;

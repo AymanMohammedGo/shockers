51feade155a1733e4d4054efdceff900dbe5ea65
AymanMohammedGo <ayhammohammed723@gmail.com>
2024-09-27 13:38:30 +0300

    change data

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "determined-bat-94d48cd643.media.strapiapp.com",
+        hostname: "original-bubble-c776909aaf.media.strapiapp.com",
         port: "", // يمكنك ترك هذا الحقل فارغًا إذا لم يكن هناك منفذ معين
         pathname: "/**", // هذا النمط يسمح بجلب الصور من جميع المسارات ضمن هذا النطاق
       },
--- utils/axiosClient.js
+++ utils/axiosClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: "http://localhost:1337/api",
-  baseURL: "https://determined-bat-94d48cd643.strapiapp.com/api",
+  baseURL: "https://original-bubble-c776909aaf.strapiapp.com/api",
 });
 
 export default instance;

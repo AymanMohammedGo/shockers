cffaca79435c0f210280ab85adbf8917a0619228
AymanMohammedGo <ayhammohammed723@gmail.com>
2024-10-09 17:45:50 +0300

    change url

--- utils/axiosClient.js
+++ utils/axiosClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: "http://localhost:1337/api",
-  baseURL: "https://tidy-broccoli-4a87d47a3b.strapiapp.com/api",
+  baseURL: "https://inviting-freedom-08306e638f.strapiapp.com/api",
 });
 
 export default instance;

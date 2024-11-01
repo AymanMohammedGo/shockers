b5e39011bf759497fd6de9031c9cee64c2f9f5ff
AymanMohammedGo <ayhammohammed723@gmail.com>
2024-11-01 22:05:25 +0300

    adding projects page

--- src/app/[locale]/ymarketing/projects/page.jsx
+++ src/app/[locale]/ymarketing/projects/page.jsx
@@ -1,14 +1,29 @@
+"use client";
 import { motion } from "framer-motion";
+import { useState, useEffect, useCallback } from "react";
+import { getProjects } from "../../../../../utils/Y_MarketingApi";
 import Image from "next/image";
-import Link from "next/link";
-import { useEffect, useState } from "react";
+const Projects = ({ params: { locale } }) => {
+  let lan = locale;
+  if (locale === "kr") {
+    lan = "af";
+  }
+  const [data, setData] = useState([]);
+  const getProjects_ = useCallback(() => {
+    getProjects(lan).then((res) => {
+      setData(res.data.data.attributes);
+    });
+    window.scrollTo(0, 2);
+  }, [lan]);
+  useEffect(() => {
+    getProjects_();
+  }, [getProjects_]);
 
-const ProjectsSection = ({ data }) => {
   const [isLoading, setIsLoading] = useState(true);
 
   const totalImages =
-    data?.images.data.map((item) => item?.attributes.url) || []; // الحصول على جميع الصور
-  const numberOfProjects = 6; // عدد الكائنات
+    data?.images?.data.map((item) => item?.attributes.url) || []; // الحصول على جميع الصور
+  const numberOfProjects = 12; // عدد الكائنات
   const imagesPerProject = Math.ceil(totalImages.length / numberOfProjects); // عدد الصور لكل كائن
 
   // خلط جميع الصور
@@ -34,7 +49,9 @@ const ProjectsSection = ({ data }) => {
     })
   );
 
-  const [projectOrder, setProjectOrder] = useState([0, 1, 2, 3, 4, 5]);
+  const [projectOrder, setProjectOrder] = useState([
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+  ]);
 
   useEffect(() => {
     const intervals = projects.map((project, index) =>
@@ -64,6 +81,34 @@ const ProjectsSection = ({ data }) => {
     }, 30000);
 
     return () => clearInterval(reorderInterval);
+  }, [data]);
+  //button scroll top
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleGoToFirstSlide = () => {
+    window.scrollTo(0, 0);
+    // if (swiperInstance) {
+    //   // document.body.style.overflow = "auto";
+    //   window.scrollTo(0, 2);
+    //   swiperInstance.update();
+    //   // swiperInstance.mousewheel.enable();
+    //   swiperInstance.slideTo(0, 2000);
+    //   // setTimeout(() => {
+    //   //   window.scrollTo(0, 0);
+    //   // }, 2000);
+    // }
+  };
+  const toggleVisibility = () => {
+    if (window.pageYOffset >= 1) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+    return () => window.removeEventListener("scroll", toggleVisibility);
   }, []);
 
   return (
@@ -71,23 +116,12 @@ const ProjectsSection = ({ data }) => {
       <div className="bg-yMarketing w-full h-full flex items-center justify-center relative z-10">
         <div className="flex w-full h-full flex-col  justify-center items-center lg:px-2 overflow-hidden">
           <div className="text-white w-full h-full flex flex-1 p-3 md:p-7 flex-col md:flex-row lg:max-h-[800px]">
-            <div className="w-full h-[400px] md:w-5/12 rounded-xl overflow-hidden relative text-white">
-              <h1 className="text-4xl sm:text-5xl lg:text-6xl xl:text-7xl font-bold mx-auto lg:my-10 my-5 ">
-                {data?.title}
-              </h1>
-              <span className="text-lg lg:text-xl xl:text-2xl">
-                {data?.des}
-              </span>
-            </div>
-            <Link
-              href="ymarketing/projects"
-              className="grid grid-cols-3 grid-rows-3 gap-3 md:gap-4 w-full md:w-7/12 h-full p-0 md:p-4"
-            >
+            <div className="grid grid-cols-6 grid-rows-3 gap-3 md:gap-4 w-full  h-full p-0 md:p-4">
               {projectOrder.map((order, i) => (
                 <motion.div
                   key={projects[order].id}
                   className={
-                    i === 0
+                    i === 0 || i == 6
                       ? "col-span-2 row-span-2 rounded-lg md:rounded-xl overflow-hidden bg-gray-200 relative"
                       : "col-span-1 row-span-1 rounded-lg md:rounded-xl overflow-hidden bg-gray-200 relative"
                   }
@@ -122,12 +156,38 @@ const ProjectsSection = ({ data }) => {
                   </motion.div>
                 </motion.div>
               ))}
-            </Link>
+            </div>
           </div>
         </div>
+      </div>
+      <div className="fixed  bottom-2 right-2 lg:bottom-8 lg:right-8 z-30">
+        {isVisible && (
+          <button
+            onClick={handleGoToFirstSlide}
+            className="p-3 rounded-full   text-white  bg-shockersAEC/20  shadow-2xl   transition-transform transform hover:scale-110 focus:outline-none "
+            style={{ transition: "transform 0.2s ease-in-out" }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="lucide lucide-circle-arrow-up"
+            >
+              <circle cx="12" cy="12" r="10" />
+              <path d="m16 12-4-4-4 4" />
+              <path d="M12 16V8" />
+            </svg>
+          </button>
+        )}
       </div>
     </section>
   );
 };
 
-export default ProjectsSection;
+export default Projects;

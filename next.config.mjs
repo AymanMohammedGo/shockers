a616fa46f005af0935ca239d27b6a9f1fe4b86db
AymanMohammedGo <ayhammohammed723@gmail.com>
2024-11-22 10:36:57 +0300

    change link

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "methodical-cabbage-84381c3703.media.strapiapp.com",
+        hostname: "honest-frogs-42f114d238.media.strapiapp.com",
         port: "", // يمكنك ترك هذا الحقل فارغًا إذا لم يكن هناك منفذ معين
         pathname: "/**", // هذا النمط يسمح بجلب الصور من جميع المسارات ضمن هذا النطاق
       },
--- utils/axiosClient.js
+++ utils/axiosClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: "http://localhost:1337/api",
-  baseURL: "https://methodical-cabbage-84381c3703.strapiapp.com/api",
+  baseURL: "https://honest-frogs-42f114d238.strapiapp.com/api",
 });
 
 export default instance;

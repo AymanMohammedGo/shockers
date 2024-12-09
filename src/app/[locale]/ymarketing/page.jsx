9bfec0d575fec8e3068aa6c8006d711fefa3208e
AymanMohammedGo <ayhammohammed723@gmail.com>
2024-12-09 08:57:38 +0300

    fix

--- src/app/[locale]/ymarketing/page.jsx
+++ src/app/[locale]/ymarketing/page.jsx
@@ -246,17 +246,12 @@ const Y_MarketingHome = ({ params: { locale } }) => {
       //swiper.mousewheel.disable();
       //document.body.style.overflow = "auto";
       swiper.mousewheel.enable();
-      // document.body.style.overflow = "hidden";
-      // window.scrollTo(0, 0);
       document.body.style.overflow = "hidden";
-      setTimeout(() => {
-        window.scrollTo(0, 0);
-        document.body.style.overflow = "auto";
-      }, 10);
+      window.scrollTo(0, 0);
     } else {
       swiper.mousewheel.enable();
       document.body.style.overflow = "hidden";
-      window.scrollTo(0, 20);
+      window.scrollTo(0, 50);
     }
   }, []);
   const handleWheel = useCallback((event) => {
